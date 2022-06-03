8bb413891533a7f34e963d4f81ba6627f4f0fcaa
Jason Anderson <moosnake1@bigpond.com>
2022-06-03 13:43:32 +0930

    Add project files.

---
+++ Code design/Dynamic Arrays Exercise/Dynamic Arrays Exercise/Dynamic Arrays Exercise.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+
+int main()
+{
+    int number = 5;
+    int size = 10;
+    Dynamic_Array* array;
+
+    
+}
+
+class Dynamic_Array
+{
+public:
+    int used = 0;
+    
+
+    Dynamic_Array()
+    {
+    }
+
+    ~Dynamic_Array() {};
+
+    void CreateArray(int size)
+    {
+
+    }
+
+    void AddElemEnd(int numb)
+    {
+        //&used++;
+        //array[&used + 1] = numb;
+    }
+};
